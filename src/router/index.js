3abe7cf53ebbc22f22ad8d96b5f8f4fb0c6dafb4
Tim Brunson <tjbrunson@gmail.com>
2020-03-24 11:18:22 -0700

    REmoved unused component and view

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import ProteinDetailsView from '../views/ProteinDetailsView.vue'
 import PathwaySearch from '../views/PathwaySearch.vue'
 
 Vue.use(VueRouter)
@@ -19,11 +18,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path:'/protein-detail/:protein_name',
-    name: "ProteinDetailsView",
-    component: ProteinDetailsView
   },
   {
     path: '/pathway-search/:proteinName',
