5e1761674b44cbaa070223b98bd8ba1553df722b
Tim Brunson <tjbrunson@gmail.com>
2020-03-24 14:54:07 -0700

    Minor change to add link to browser portal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <v-content class="searchContainer" fluid>
+    <v-container class="searchContainer" fluid>
       <ProteinSearchForm v-on:search-protein="searchProtein" />
-      <v-container>
-
-      </v-container>
-    </v-content>
+    </v-container>
+    <v-container ma-5>
+      <v-card class="mx-auto" max-width="400px" raised>
+        <v-card-title>
+          <span class="title text-center font-weight-medium">Lauch the IDG Pathway Browser</span>
+        </v-card-title>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
@@ -16,7 +20,7 @@ import router from "@/router/index.js";
 export default {
   name: "Home",
   components: {
-    ProteinSearchForm,
+    ProteinSearchForm
   },
   methods: {
     searchProtein(proteinName) {
@@ -31,6 +35,7 @@ export default {
 
 <style scoped>
 .searchContainer {
+  border-top: 1px solid #c0c0c0;
   background-color: #e0e0e0;
 }
 </style>
