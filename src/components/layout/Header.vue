e5998572ade48f4958d5993d7647ca4754109d00
Tim Brunson <tjbrunson@gmail.com>
2020-03-27 09:08:23 -0700

    Added docs link to header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -4,6 +4,7 @@
       <router-link to="/">
         <img :src="'/assets/images/logo.svg'" alt="Reactome Logo" class="logo"/>
       </router-link>
+      <v-container class="links"><router-link to="/documentation">Docs</router-link></v-container>
     </v-container>
   </header>
 </template>
@@ -24,5 +25,16 @@ a {
 .logo-container{
   text-align: left;
   border-bottom: 1px solid #c0c0c0;
+}
+.links {
+  float: right;
+  color:black;
+  height:100%;
+}
+.links a {
+  float: right;
+  font-weight: medium;
+  text-decoration: none;
+  height:100%;
 }
 </style>
