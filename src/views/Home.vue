4b15d60fe3157d86e2ed0d3ac1476459a87920bf
Tim Brunson <tjbrunson@gmail.com>
2020-05-12 10:08:22 -0700

    rework to one column home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,133 +1,139 @@
 <template>
   <v-container fluid>
-    <v-row dense>
-      <!-- LEFT SIDE CONTAINER -->
-      <v-col col="12" md="8" sm="12" xs="12">
-        <v-container fluid>
-
-          <v-card class="mb-5" dark raised>
-            <v-card-title>Illuminating the Druggable Genome with Reactome</v-card-title>
+    <v-container fluid>
+      <v-card class="mb-5" dark raised>
+        <v-card-title>
+          Illuminating the Druggable Genome with Reactome
+          <v-spacer></v-spacer>
+          <v-btn icon @click="show = !show">
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-expand-transition>
+          <div v-show="show">
             <v-card-text>
               <p
                 class="text-left"
               >At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
             </v-card-text>
-          </v-card>
-          
-          <v-card dark raised>
-            <v-card-title>
-              <v-text-field
-                dark
-                label="Search a protein"
-                v-model="search"
-                placeholder="e.g. O95631, NTN1, signaling by EGFR, glucose"
-                :outlined="true"
-                class="searchContainer"
-                v-on:keydown.enter="searchProtein"
-              ></v-text-field>
+          </div>
+        </v-expand-transition>
+      </v-card>
+    </v-container>
+    <v-container fluid>
+      <v-card dark raised class="pa-5">
+        <v-card-title class="pa-0">IDG Reactome</v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" md="4">
+          <v-card class="mx-auto mt-5" light max-width="250px" height="90%" raised>
+            <v-card-title class="justify-center">
+              <a :href="browserLink">
+                <v-btn
+                  large
+                  color="primary"
+                  height="50px"
+                  width="100px"
+                  class="navigationButton"
+                >Launch!</v-btn>
+              </a>
             </v-card-title>
             <v-card-text>
-              <v-container v-if="pathways && pathways.length > 0">
-                <v-data-table
-                  :headers="headers"
-                  :items="pathways"
-                  :expanded.sync="expanded"
-                  :footer-props="{'items-per-page-options': [5,40,50,100,-1]}"
-                  show-expand
-                >
-                  <template
-                    v-slot:item.description="{item}"
-                  >{{item.description.substring(0, 30) + "..."}}</template>
-                  <template v-slot:expanded-item="{headers, item}">
-                    <td :colspan="headers.length">
-                      <p class="text-left">
-                        <b>Description</b>
-                      </p>
-                      <p class="text-left">{{item.description}}</p>
-                      <v-container v-if="item.overlayEntities && item.overlayEntities.length > 0">
-                        <v-row>
-                          <v-col
-                            v-for="(entity, index) in item.overlayEntities"
-                            :key="index"
-                            cols="6"
-                            md="2"
-                            sm="4"
-                            class="small-text"
-                          >{{entity}}</v-col>
-                        </v-row>
-                      </v-container>
-                    </td>
-                  </template>
-                </v-data-table>
+              <v-container>
+                <p class="font-weight-bold">Lauch the IDG Pathway Browser</p>
               </v-container>
             </v-card-text>
           </v-card>
-        </v-container>
-      </v-col>
-      <!-- RIGHT SIDE CONTAINER -->
-      <v-col col="12" md="4" sm="12" xs="12">
-        <v-container fluid>
-          <v-card dark raised>
-            <v-card-title class="mx-auto">IDG Reactome</v-card-title>
+          </v-col>
+          <v-col cols="12" md="4">
+          <v-card class="mx-auto mt-5" light max-width="250px" height="90%" raised>
+            <v-card-title class="justify-center">
+              <a v-bind:href="reactomeLink">
+                <v-btn
+                  large
+                  color="primary"
+                  height="50px"
+                  width="100px"
+                  class="navigationButton"
+                >Go!</v-btn>
+              </a>
+            </v-card-title>
             <v-card-text>
-              <v-card class="mx-auto mt-5" light max-width="300px" height="100%" raised>
-                <v-card-title class="justify-center">
-                  <a :href="browserLink">
-                    <v-btn
-                      large
-                      color="primary"
-                      height="50px"
-                      width="100px"
-                      class="navigationButton"
-                    >Launch!</v-btn>
-                  </a>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <p class="title font-weight-medium">Lauch the IDG Pathway Browser</p>
-                  </v-container>
-                </v-card-text>
-              </v-card>
-              <v-card class="mx-auto mt-5" light max-width="300px" height="100%" raised>
-                <v-card-title class="justify-center">
-                  <a v-bind:href="reactomeLink">
-                    <v-btn
-                      large
-                      color="primary"
-                      height="50px"
-                      width="100px"
-                      class="navigationButton"
-                    >Go!</v-btn>
-                  </a>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <p class="title font-weight-medium">Vist Reactome</p>
-                  </v-container>
-                </v-card-text>
-              </v-card>
-              <v-card class="mx-auto mt-5" light max-width="300px" height="100%" raised>
-                <v-card-title class="justify-center">
-                  <router-link to="/documentation">
-                    <v-btn
-                      color="primary"
-                      height="50px"
-                      width="100px"
-                      class="navigationButton"
-                    >View!</v-btn>
-                  </router-link>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <p class="title font-weight-medium">Documentation</p>
-                  </v-container>
-                </v-card-text>
-              </v-card>
+              <v-container>
+                <p class="font-weight-bold">Vist Reactome</p>
+              </v-container>
             </v-card-text>
           </v-card>
-        </v-container>
-      </v-col>
-    </v-row>
+          </v-col>
+
+          <v-col cols="12" md="4">
+          <v-card class="mx-auto mt-5" light max-width="250px" height="90%" raised>
+            <v-card-title class="justify-center">
+              <router-link to="/documentation">
+                <v-btn color="primary" height="50px" width="100px" class="navigationButton">View!</v-btn>
+              </router-link>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <p class="font-weight-bold">Documentation</p>
+              </v-container>
+            </v-card-text>
+          </v-card>
+          </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-container>
+    <v-container fluid>
+      <v-card dark raised>
+        <v-card-title >
+          <v-text-field
+            dark
+            label="Search a protein"
+            v-model="search"
+            placeholder="e.g. O95631, NTN1, signaling by EGFR, glucose"
+            :outlined="true"
+            class="searchContainer"
+            v-on:keydown.enter="searchProtein"
+          ></v-text-field>
+        </v-card-title>
+        <v-card-text>
+          <v-container v-if="pathways && pathways.length > 0">
+            <v-data-table
+              :headers="headers"
+              :items="pathways"
+              :expanded.sync="expanded"
+              :footer-props="{'items-per-page-options': [5,40,50,100,-1]}"
+              show-expand
+            >
+              <template
+                v-slot:item.description="{item}"
+              >{{item.description.substring(0, 30) + "..."}}</template>
+              <template v-slot:expanded-item="{headers, item}">
+                <td :colspan="headers.length">
+                  <p class="text-left">
+                    <b>Description</b>
+                  </p>
+                  <p class="text-left">{{item.description}}</p>
+                  <v-container v-if="item.overlayEntities && item.overlayEntities.length > 0">
+                    <v-row>
+                      <v-col
+                        v-for="(entity, index) in item.overlayEntities"
+                        :key="index"
+                        cols="6"
+                        md="2"
+                        sm="4"
+                        class="small-text"
+                      >{{entity}}</v-col>
+                    </v-row>
+                  </v-container>
+                </td>
+              </template>
+            </v-data-table>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-container>
   </v-container>
 </template>
 
@@ -146,7 +152,8 @@ export default {
     expanded: [],
     pathways: [],
     search: "",
-    proteinName: ""
+    proteinName: "",
+    show: false
   }),
   methods: {
     searchProtein() {
