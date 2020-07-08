e5df3e0a8626bab74ed73bf609d0fd0f418008a2
Tim Brunson <tjbrunson@gmail.com>
2020-07-08 11:02:39 -0700

    cleanup for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -14,22 +14,20 @@ const routes = [
     }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: "/documentation",
     name: "Documentation",
-    component: () => import ('../views/Docs/Documentation.vue')
+    component: () => import ('../views/Docs/Documentation.vue'),
+    meta: {
+      title: "Docs"
+    }
   },
   {
     path: "/cite",
     name: "Cite",
-    component: () => import ('../views/Docs/Cite.vue')
+    component: () => import ('../views/Docs/Cite.vue'),
+    meta: {
+      title: "Cite Us"
+    }
   },
 ]
 
