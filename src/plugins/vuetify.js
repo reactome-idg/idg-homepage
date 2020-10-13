ffac4edde5a4c7ef4ca726e4acccf36348c59378
Tim Brunson <tjbrunson@gmail.com>
2020-10-13 14:14:50 -0700

    revert change

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,17 +4,4 @@ import Vuetify from "vuetify/lib";
 Vue.use(Vuetify);
 const vuetify = new Vuetify({});
 
-if (process.env.VUE_APP_WEB_COMPONENT === "true") {
-  vuetify.framework.theme.checkOrCreateStyleElement = function() {
-    if (!this.styleEl) this.genStyleElement();
-    return Boolean(this.styleEl);
-  };
-
-  // Monkey patch querySelector to properly find root element
-  const { querySelector } = document;
-  document.querySelector = function(selector) {
-    return querySelector.call(this, selector);
-  };
-}
-
 export default vuetify;
