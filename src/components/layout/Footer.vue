9e760150b077cfabfe1b3473d8f64b3b0433614e
Tim Brunson <tjbrunson@gmail.com>
2020-10-26 14:35:47 -0700

    updated style

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <v-footer absolute color="#222">
       <v-spacer></v-spacer>
-      <div class="font-weight-medium">&copy; Reactome {{ new Date().getFullYear() }}</div>
+      <div class="font-weight-medium text-white" >&copy; Reactome {{ new Date().getFullYear() }}</div>
     </v-footer>
 </template>
 
@@ -10,4 +10,7 @@ export default {};
 </script>
 
 <style scoped>
+.text-white{
+  color: white;
+}
 </style>
