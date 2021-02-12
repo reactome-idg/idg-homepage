aa8392c959abd24b0e991de1e8373d2295d55a65
Tim Brunson <tjbrunson@gmail.com>
2021-02-12 11:28:52 -0800

    added validation to only fire event when at least 1 gene exists

--- src/components/features/PairwiseSearch/FuncInteractionScoreFilter.vue
+++ src/components/features/PairwiseSearch/FuncInteractionScoreFilter.vue
@@ -10,10 +10,11 @@
         min="0"
         max="1"
         step="0.1"
-        :suffix="numberOfGenes"
+        :suffix="numberOfGenesLabel"
       ></v-text-field>
+      <p style="color:red;">{{error}}</p>
     </v-col>
-    <v-col cols="3" align-self="end">
+    <v-col cols="3" align-self="center">
       <v-btn class="ml-2" small @click="updatePRD">UPDATE</v-btn>
     </v-col>
   </v-row>
@@ -30,17 +31,27 @@ export default {
   },
   data: () => ({
     prd: 0.9,
+    error: ""
   }),
   computed: {
       numberOfGenes() {
-          return this.interactingGenes !== null ?
-           this.interactingGenes.filter(({score}) => score>= this.prd).length + " genes" :
-           "Loading..."
+          return this.interactingGenes !== null &&
+           this.interactingGenes.filter(({score}) => score>= this.prd).length
+      },
+      numberOfGenesLabel() {
+        if(!this.interactingGenes) return "Loading...";
+        return this.numberOfGenes + " genes";
       }
   },
   methods: {
       updatePRD() {
+        if(this.numberOfGenes > 0){
+          this.error = "";
           this.$emit('updatePRD', this.prd);
+        }
+        else{
+            this.error = "Please choose a cutoff with at least 1 gene";
+        }
       }
   }
 };
