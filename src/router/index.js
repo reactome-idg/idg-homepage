422bef4437e21b0a24f53f5a9c7e1bd3f3e80836
Tim Brunson <tjbrunson@gmail.com>
2021-02-16 12:01:29 -0800

    added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import NotFound from '../views/error/NotFound.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
     meta: {
-      title: "Home"
-    }
+      title: "Home",
+    },
   },
   {
     path: "/documentation",
     name: "Documentation",
-    component: () => import ('../views/Docs/Documentation.vue'),
+    component: () => import("../views/Docs/Documentation.vue"),
     meta: {
-      title: "Docs"
-    }
+      title: "Docs",
+    },
   },
   {
     path: "/documentation/userguide",
     name: "User Guide",
-    component: () => import ('../views/Docs/UserGuide.vue'),
+    component: () => import("../views/Docs/UserGuide.vue"),
     meta: {
-      title: "User Guide"
-    }
+      title: "User Guide",
+    },
   },
   {
     path: "/cite",
     name: "Cite",
-    component: () => import ('../views/Docs/Cite.vue'),
+    component: () => import("../views/Docs/Cite.vue"),
     meta: {
-      title: "Cite Us"
-    }
+      title: "Cite Us",
+    },
   },
-]
+  {
+    path: "/:catchAll(.*)",
+    name: "Not Found",
+    component: NotFound,
+    meta: {
+      title: "Error",
+    },
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
---
+++ src/views/error/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container flex">
+    <div>
+      <h1>Page not found!</h1>
+      <router-link to="/">
+        <v-btn color="primary"> Go Home </v-btn>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  beforeCreate: () => {
+    document.body.className = "normal";
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 1100px;
+  height: 100%;
+  margin: 0 auto;
+}
+.flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+a {
+  text-decoration: none;
+}
+</style>
