a7579c1c133abf211ed361e714df40226dc91ef1
Tim Brunson <tjbrunson@gmail.com>
2021-05-06 14:10:21 -0700

    user guide markdown rendering works

--- src/views/Docs/UserGuide.vue
+++ src/views/Docs/UserGuide.vue
@@ -1,20 +1,25 @@
 <template>
   <v-main class="mb-5">
     <v-container class="text-left mb-5">
-      <userGuide />
+      <h1>User Guide</h1>
+      <div id="test" class="mb-5 pl-3"></div>
+      <vue-markdown :source="userGuide" :toc="true" :toc-anchor-link="false" toc-id="test"></vue-markdown>
     </v-container>
   </v-main>
 </template>
 
 <script>
+import userGuide from 'raw-loader!../../../public/assets/doc/text/userGuide.md';
 export default {
   name: "UserGuide",
   components: {
-    userGuide: () => import('../../components/documentation/userGuide.md'),
+    VueMarkdown: () => import('vue-markdown')
   },
-  data: () => ({}),
+  data: () => ({
+    userGuide,
+  }),
 };
 </script>
 
-<style scoped>
+<style>
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-  chainWebpack: config => {
-    config.module.rule('md')
-      .test(/\.md/)
-      .use('vue-loader')
-      .loader('vue-loader')
-      .end()
-      .use('vue-markdown-loader')
-      .loader('vue-markdown-loader/lib/markdown-compiler')
-      .options({
-        raw: true
-      })
-  },
   assetsDir: "./idg_assets",
   transpileDependencies: [
     "vuetify"
