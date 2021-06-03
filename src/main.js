933add0bc3f5e962cf947cd5979d61c6ab797edc
Tim Brunson <tjbrunson@gmail.com>
2021-06-03 12:09:29 -0700

    added google analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import VueGtag from "vue-gtag";
 
-
+Vue.use(VueGtag, {
+  config: { id: "UA-42985898-5" },
+});
 Vue.config.productionTip = false
 const app = 
 new Vue({
