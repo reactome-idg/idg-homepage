fd3f3d1fa3ee2756809dcbc44c445323f67fe51e
Tim Brunson <tjbrunson@gmail.com>
2021-07-16 08:39:33 -0700

    added cytoscape

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import VueGtag from "vue-gtag";
+import cytoscape from 'cytoscape'
+import popper from 'cytoscape-popper'
+
+cytoscape.use(popper)
 
 //google analytics setup
 Vue.use(VueGtag, {
