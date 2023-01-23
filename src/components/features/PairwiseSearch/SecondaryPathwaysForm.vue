25f37a209cb76cbe5ca3fa399cea949136ed4592
Deidre Beavers <107586218+beaversd@users.noreply.github.com>
2023-01-23 09:12:39 -0800

    Both plots

--- src/components/features/PairwiseSearch/SecondaryPathwaysForm.vue
+++ src/components/features/PairwiseSearch/SecondaryPathwaysForm.vue
@@ -192,6 +192,13 @@ export default {
     else
       throw new Error("No data descriptions can be found.");
   },
+  // async created() {
+  //   try {
+  //     this.dataDescs = await PairwiseService.getDataDescs(this.term);
+  //   } catch (err) {
+  //     this.error = err.message;
+  //   }
+  // },
   methods: {
     searchSecondaryPathways() {
       if (this.relationshipTypes.length < 1) {
